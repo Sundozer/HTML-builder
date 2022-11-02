480f24c99396346c29af28efc79bd738d2be90cd
Alex Hramovich <xtr1m@mail.ru>
2022-11-02 16:29:51 +0300

    fix: add access to file from root

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const fs = require('fs');
-const read = fs.createReadStream('text.txt', 'utf-8');
+a = path.join(__dirname, 'text.txt');
+
+
+const read = fs.createReadStream(a, 'utf-8');
 read.on('data', chunk => console.log(chunk));
 read.on('end', () => console.log('End'));
 read.on('error', error => console.log('Error', error.message));
