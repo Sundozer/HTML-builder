07098110550bc465ebca52ae533c9ad4d41a0b8f
Alex Hramovich <xtr1m@mail.ru>
2022-11-04 16:32:42 +0300

    feat: add new function - copy files to new folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,7 +1,25 @@
 const path = require('path');
 const fs = require('fs');
 fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
-    if (err) {
-        throw err
-    }
+    // if (err) {
+    //     throw err;
+    // }
+    let p = path.join(__dirname, 'files')
+    fs.readdir(p, (err, files) => {
+        files.forEach(file => {
+            let res = path.join(p, file)
+            copy(p, res)
+        })
+    })
+    
 })
+
+function copy (folder, data) {
+    let newFolder = path.join(folder+'-copy\\'+path.basename(data))
+    fs.copyFile(data, newFolder, (err) => {
+        // if (err) {
+        // throw err;
+        // }
+        
+    });
+}
