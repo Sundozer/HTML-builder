4e2ae82b72569f6b95c62b58bbdca1f23ae21435
Alex Hramovich <xtr1m@mail.ru>
2022-11-04 15:39:42 +0300

    fix: upgrade information with async function

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -9,20 +9,22 @@ fs.readdir(pathToFile, { withFileTypes: true }, (err, files) => {
         files.forEach(file => {
             if (file.isDirectory() !== true) {
                 let p = path.join(__dirname, 'secret-folder', file.name);
-                console.log(path.parse(file.name).name, '-', path.extname(file.name).slice(1), '-', fs.statSync(p).size/*statt(file.name)*/)
+                // let result = `${path.parse(file.name).name} - ${path.extname(file.name).slice(1)} - ${statt(file.name)}`; /*fs.statSync(p).size*/
+                // console.log(result)
+                let result = `${path.parse(file.name).name} - ${path.extname(file.name).slice(1)} - `;
+                statt(file.name, result)
             }
-            
         })
     }
 })
-// function statt (data) {
-//     let p = path.join(__dirname, 'secret-folder', data);
-//     let siz;
-//     fs.stat(p, (error, stats) => {
-//         siz = stats.size;
-//         return siz;
-//     });
-    
-// }
+function statt (data, res) {
+    let p = path.join(__dirname, 'secret-folder', data);
+    let siz;
+    fs.stat(p, (error, stats) => {
+        siz = stats.size;
+        console.log(res, siz)
+    });
+}
+
 
 
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,7 @@
-
+const path = require('path');
+const fs = require('fs');
+fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
+    if (err) {
+        throw err
+    }
+})
