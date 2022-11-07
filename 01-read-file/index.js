95100e0bbb169025237a9426e20381784f07e979
Alex Hramovich <xtr1m@mail.ru>
2022-11-07 15:36:37 +0300

    refactor: change end frase at first task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -5,5 +5,5 @@ a = path.join(__dirname, 'text.txt');
 
 const read = fs.createReadStream(a, 'utf-8');
 read.on('data', chunk => console.log(chunk));
-read.on('end', () => console.log('End'));
+read.on('end', () => console.log('Goodbye!'));
 read.on('error', error => console.log('Error', error.message));
